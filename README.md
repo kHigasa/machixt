8677196295f41023ad0748d3f450d5ab7a480d90
kHigasa <kojihigasa@gmail.com>
2018-12-03 21:21:19 +0900

    Install ts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # machixt
 
-> frontend of machix
+> Nuxt.js TypeScript project
 
 ## Build Setup
 
@@ -20,4 +20,3 @@ $ npm run generate
 ```
 
 For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,37 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        machixt
-      </h1>
-      <h2 class="subtitle">
-        frontend of machix
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <section>
+    <h1 class="header">Nuxt TypeScript Starter</h1>
+    <div class="cards">
+      <Card v-for="person in people" :key="person.id" :person="person"></Card>
     </div>
   </section>
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import Card from "~/components/Card.vue"
 
-export default {
+@Component({
   components: {
-    AppLogo
+    Card
   }
+})
+export default class extends Vue {
+  @State people
 }
 </script>
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
+    sans-serif;
+}
 
-<style>
-.container {
-  min-height: 100vh;
+.cards {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  flex-wrap: wrap;
 }
 </style>
-
